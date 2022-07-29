d30fa65be97576edbca04e00c581cddcc5db08fb
kz56cd <kz56cd@gmail.com>
2022-07-29 16:12:07 +0900

    Init project

---
+++ preSpeechRecognition/preSpeechRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  preSpeechRecognitionApp.swift
+//  preSpeechRecognition
+//
+//  Created by Masakazu Sano on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct preSpeechRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
